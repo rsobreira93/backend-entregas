b42ccbe7c548dd68227929c995f292d01a126491
rsobreira93 <sobreiraromuloes@gmail.com>
2022-04-27 14:06:32 -0300

    feat: add expess-async

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import Express from "express";
+import "express-async-errors";
+import Express, { NextFunction, Request, Response } from "express";
 import { routes } from "./routes";
 
 const app = Express();
@@ -6,5 +7,24 @@ const app = Express();
 app.use(Express.json());
 
 app.use(routes);
+
+app.use(
+  (
+    err: Error,
+    request: Request,
+    response: Response,
+    nextFunction: NextFunction
+  ) => {
+    if (err instanceof Error) {
+      return response.status(400).json({
+        message: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      message: "Internal server error",
+    });
+  }
+);
 
 app.listen(3000, () => console.log("Server is running on port 3000"));
