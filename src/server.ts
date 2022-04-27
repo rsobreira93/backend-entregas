0e4b6e5d31fb1ba2b159acf631f0a979716cb779
rsobreira93 <sobreiraromuloes@gmail.com>
2022-04-27 11:00:19 -0300

    feat: add json and routes to server

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,10 @@
 import Express from "express";
+import { routes } from "./routes";
 
 const app = Express();
+
+app.use(Express.json());
+
+app.use(routes);
 
 app.listen(3000, () => console.log("Server is running on port 3000"));
