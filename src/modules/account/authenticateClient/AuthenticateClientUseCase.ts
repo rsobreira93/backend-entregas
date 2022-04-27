4169b57fc2ffa1c165bb15bfe9219669705005d2
rsobreira93 <sobreiraromuloes@gmail.com>
2022-04-27 13:42:35 -0300

    feat: add AuthenticateUseCase

---
+++ src/modules/account/authenticateClient/AuthenticateClientUseCase.ts
@@ -0,0 +1,37 @@
+import { prisma } from "../../../database/prismaClient";
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+
+interface IAuthenticateClient {
+  username: string;
+  password: string;
+}
+
+export class AuthenticateClientUseCase {
+  async execute({ password, username }: IAuthenticateClient) {
+    const client = await prisma.clients.findFirst({
+      where: {
+        username: {
+          mode: "insensitive",
+        },
+      },
+    });
+
+    if (!client) {
+      throw new Error("username or password incorrect");
+    }
+
+    const passwordMatch = await compare(password, client.password);
+
+    if (!passwordMatch) {
+      throw new Error("username or password incorrect");
+    }
+
+    const token = sign({ username }, "dea12ffb2578f2789b8bde3d446f3415", {
+      subject: client.id,
+      expiresIn: "1d",
+    });
+
+    return token;
+  }
+}
