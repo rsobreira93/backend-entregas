93caca8bf82dac03e4ac74506eb561ba0b3c3df3
rsobreira93 <sobreiraromuloes@gmail.com>
2022-04-27 13:43:29 -0300

    feat: add AuthenticateClientController

---
+++ src/modules/account/authenticateClient/AuthenticateClientController.ts
@@ -0,0 +1,17 @@
+import { AuthenticateClientUseCase } from "../authenticateClient/AuthenticateClientUseCase";
+import { Request, Response } from "express";
+
+export class AuthenticateClientController {
+  async handle(request: Request, response: Response) {
+    const { username, password } = request.body;
+
+    const authenticateClientUseCase = new AuthenticateClientUseCase();
+
+    const result = await authenticateClientUseCase.execute({
+      username,
+      password,
+    });
+
+    return response.json(result);
+  }
+}
