9b45cf78be55ce4f408485567e20f1cda6b269a1
rsobreira93 <sobreiraromuloes@gmail.com>
2022-04-28 07:40:38 -0300

    feat: add AuthenticateDeliverymanUseCase

---
+++ src/modules/account/authenticateDeliveryman/AuthenticateDeliverymanUseCase.ts
@@ -0,0 +1,37 @@
+import { prisma } from "../../../database/prismaClient";
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+
+interface IAuthenticateDeliveryman {
+  username: string;
+  password: string;
+}
+
+export class AuthenticateDeliverymanUseCase {
+  async execute({ password, username }: IAuthenticateDeliveryman) {
+    const deliveryman = await prisma.deliveryman.findFirst({
+      where: {
+        username: {
+          mode: "insensitive",
+        },
+      },
+    });
+
+    if (!deliveryman) {
+      throw new Error("username or password incorrect");
+    }
+
+    const passwordMatch = await compare(password, deliveryman.password);
+
+    if (!passwordMatch) {
+      throw new Error("username or password incorrect");
+    }
+
+    const token = sign({ username }, "dea12ffb2578f7789b8bde3d446f3415", {
+      subject: deliveryman.id,
+      expiresIn: "1d",
+    });
+
+    return token;
+  }
+}
