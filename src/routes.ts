c9548d473466c40e9fc701031ba59b4bc364fafb
rsobreira93 <sobreiraromuloes@gmail.com>
2022-04-28 08:10:14 -0300

    feat: add delivery route

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { AuthenticateClientController } from "./modules/account/authenticateClie
 import { Router } from "express";
 import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientController";
 import { CreateDeliverymanController } from "./modules/deleveyman/useCases/createDeliveryman/CreateDeliverymanController";
+import { CreateDeliveryController } from "./modules/deliveries/useCases/createDelivery/CreateDeliveryController";
 
 const routes = Router();
 
@@ -11,6 +12,7 @@ const authenticateClientController = new AuthenticateClientController();
 const createDeliverymanController = new CreateDeliverymanController();
 const authenticateDeliverymanController =
   new AuthenticateDeliverymanController();
+const createDeliveryController = new CreateDeliveryController();
 
 routes.post("/client/authenticate", authenticateClientController.handle);
 routes.post(
@@ -20,5 +22,7 @@ routes.post(
 
 routes.post("/client", createClientController.handle);
 routes.post("/deliveryman", createDeliverymanController.handle);
+
+routes.post("/delivery", createDeliveryController.handle);
 
 export { routes };
