8da276ed499f17b3f03649ec56231da9b611112b
rsobreira93 <sobreiraromuloes@gmail.com>
2022-04-28 08:49:56 -0300

    feat: add middleware client

---
+++ src/middlewares/ensureAuthenticateClient.ts
@@ -0,0 +1,33 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+interface IPayload {
+  sub: string;
+}
+
+export async function ensureAuthenticateClient(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    return response.status(401).json({ message: "Token missing" });
+  }
+
+  const [, token] = authHeader.split(" ");
+
+  try {
+    const { sub } = verify(
+      token,
+      "dea12ffb2578f2789b8bde3d446f3415"
+    ) as IPayload;
+
+    request.id_client = sub;
+
+    return next();
+  } catch (err) {
+    return response.status(401).json({ message: "Token invalid" });
+  }
+}
