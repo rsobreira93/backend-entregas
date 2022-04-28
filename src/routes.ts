7eed920ad78f163780edfbd3e2dde65d94a8f4c7
rsobreira93 <sobreiraromuloes@gmail.com>
2022-04-28 07:12:09 -0300

    feat: add deliveryman route

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,16 @@
 import { AuthenticateClientController } from "./modules/account/authenticateClient/AuthenticateClientController";
 import { Router } from "express";
 import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientController";
+import { CreateDeliverymanController } from "./modules/deleveyman/useCases/createDeliveryman/CreateDeliverymanController";
 
 const routes = Router();
 
 const createClientController = new CreateClientController();
 const authenticateClientController = new AuthenticateClientController();
+const createDeliverymanController = new CreateDeliverymanController();
 
 routes.post("/client", createClientController.handle);
 routes.post("/authenticate", authenticateClientController.handle);
+routes.post("/deliveryman", createDeliverymanController.handle);
 
 export { routes };
